acaa6580fa2bfc3c69c9bf461b25ff6b6ab20534
magedsopihy <magedsopihy@gmail.com>
2022-03-25 14:56:30 +0200

    final

--- app/script.groovy
+++ app/script.groovy
@@ -8,7 +8,7 @@ def buildApp(){
 def buildImage(){
     echo "building the docker image"
     withCredentials([usernamePassword(credentialsId: 'DOCKER_HUB', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]){
-    sh 'docker build -t magedsopihy/node_project:1.0 -f /app/Dockerfile'
+    sh 'docker build -t magedsopihy/node_project:1.0 .'
     sh 'echo $PASS | docker login -u $USERNAME --password-stdin'
     sh 'docker push magedsopihy/node_project:1.0'
     }
